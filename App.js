80a06eb59519f42f290cc9006449b6b47eb07f5a
Harshit Gajjar <Harshit@Harshits-MacBook-Pro.local>
2020-04-18 12:11:14 +0400

    modified youssef code UI

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ export default function App(){
               {/* <Stack.Screen name="Login" component={Login} />
               // <Stack.Screen name="SignIn" component={SignIn} /> */}
 
-              <Stack.Screen name="profile" component={profile2} />
+              {/* <Stack.Screen name="profile" component={Payment} /> */}
 
               <Stack.Screen name="signin" component={SignIn} />
               <Stack.Screen name="login" component={Login} />
